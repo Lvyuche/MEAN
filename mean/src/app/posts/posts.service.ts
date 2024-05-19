ba90a139a7d2f8f9e5ba8b8ea610ae82211d7874
Yuxuan Lyu <yuxuanly@usc.edu>
2024-05-19 16:26:36 -0700

    Observables

--- mean/src/app/posts/posts.service.ts
+++ mean/src/app/posts/posts.service.ts
@@ -1,20 +1,26 @@
 import { Injectable } from '@angular/core';
 import { Post } from './post.model';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostsService {
   private posts: Post[] = [];
+  private postsUpdated = new Subject<Post[]>();
 
-  constructor() { }
 
   getPosts() {
     return [...this.posts];  // return a new array with the same elements
   }
 
+  getPostUpdateListener() {
+    return this.postsUpdated.asObservable();
+  }
+
   addPost(title: string, content: string) {
     const post: Post = { title, content };
     this.posts.push(post);
+    this.postsUpdated.next([...this.posts]);
   }
 }
